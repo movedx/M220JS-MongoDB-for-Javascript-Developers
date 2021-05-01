27a1868af8618149811a3b5ecaee5b28a500868e
movedx <grytsenko.bogdan@gmail.com>
2021-05-01 23:10:36 +0200

    Ticket: Handling Errors

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -338,6 +338,7 @@ export default class MoviesDAO {
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
+      if (e.message.indexOf("InvalidId") !== 0) return null
       console.error(`Something went wrong in getMovieByID: ${e}`)
       throw e
     }
